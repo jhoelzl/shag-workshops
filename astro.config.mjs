008cdf74f42f1c35003ad1434de13996cd57bcd7
Josef Hölzl <j.hoelzl@commend.com>
2026-05-22 14:52:44 +0200

    fix: data loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://shagadeus.at',
   base: '/',
   output: 'static',
   integrations: [react()],
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
